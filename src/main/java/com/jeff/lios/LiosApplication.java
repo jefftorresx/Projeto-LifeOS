d5889875d769cdd915b49f77b7f2a8128c829080
Jeffrey <jeff.dsv@gmail.com>
2026-08-17 19:31:58 -0400

    Primeira parte do projeto

---
+++ src/main/java/com/jeff/lios/LiosApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.lios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeff/lios/LiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.lios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
